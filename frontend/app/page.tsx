2bd9cfb1e7b477918ed4f4ea83b4d6043fe21506
theunhackable <srirangasaipagala@gmail.com>
2024-01-02 08:48:19 +0530

    added next.js for the frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className='text-2xl font-bold'>
+        Task Manager
+      </h1>
+    </main>
+  )
+}
