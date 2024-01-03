c501406079503715ccbb20cebd9da7a5ed347967
theunhackable <srirangasaipagala@gmail.com>
2024-01-03 09:24:26 +0530

    added nav bar

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import TopBar from '@/components/navbar/TopBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className} >
+
+        <TopBar />
+        
+
+        {children}
+      </body>
     </html>
   )
 }
