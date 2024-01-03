82b8ec166c63e410143936175bcec7a86aec8553
theunhackable <srirangasaipagala@gmail.com>
2024-01-03 09:25:04 +0530

    added basic functionality

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,99 @@
+"use client";
+import CreateTask from "@/components/task/CreateTask";
+import TaskItem from "@/components/task/TaskItem";
+import { apiGet } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [name, setName] = useState<string | null>();
+  const [email, setEmail] = useState<string | null>();
+  const router = useRouter();
+
+  if (typeof window !== "undefined" && !localStorage.getItem("token"))
+    router.push("/signin");
+
+  useEffect(() => {
+    async function fetchTasks() {
+      if (typeof window !== "undefined") {
+        setName((prev) => localStorage.getItem("name"));
+        setEmail((prev) => localStorage.getItem("email"));
+      }
+      try {
+        const res = await apiGet("http://127.0.0.1:4000/tasks", true);
+        setTasks(res.data);
+      } catch (e: any) {
+        alert(e.message);
+      }
+    }
+    fetchTasks();
+  }, []);
+
+  const updateTasks = (todo_id: string, field: string, updated: string) => {
+    if (field === "title") {
+      setTasks((prev_tasks) => {
+        const updated_tasks = prev_tasks.map((prev_task) => {
+          return prev_task._id === todo_id
+            ? { ...prev_task, title: updated }
+            : prev_task;
+        });
+        return updated_tasks;
+      });
+    } else if (field === "desc") {
+      setTasks((prev_tasks) => {
+        const updated_tasks = prev_tasks.map((prev_task) => {
+          return prev_task._id === todo_id
+            ? { ...prev_task, desc: updated }
+            : prev_task;
+        });
+        return updated_tasks;
+      });
+    } else if (field === "status")
+      setTasks((prev_tasks) => {
+        const updated_tasks = prev_tasks.map((prev_task) => {
+          return prev_task._id === todo_id
+            ? { ...prev_task, status: updated }
+            : prev_task;
+        });
+        return updated_tasks;
+      });
+  };
+  const deleteTask = (todo_id: string) => {
+    setTasks((prev_tasks) =>
+      prev_tasks.filter((prev_task) => prev_task._id != todo_id)
+    );
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className='text-2xl font-bold'>
-        Task Manager
-      </h1>
+    <main className="">
+      <div className="px-4 py-6 mx-auto my-5 max-w-2xl">
+        <div className="flex items-center justify-between mb-2">
+          <div>
+            <h1 className="text-2xl font-bold">welcome {name}</h1>
+            <p className="text-gray-500 text-sm">{email}</p>
+          </div>
+        </div>
+        <CreateTask setTasks={setTasks} />
+        <div className="my-10">
+          {tasks?.length !== 0 ? (
+            tasks?.map((task: Task) => (
+              <TaskItem
+                key={task._id}
+                task={task}
+                updateTasks={updateTasks}
+                deleteTask={deleteTask}
+              />
+            ))
+          ) : (
+            <div>
+              <p className="text-center font-semibold text-gray-400">
+                No tasks to show. Click on `Create task` button to create a new
+                task.
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
     </main>
-  )
+  );
 }
